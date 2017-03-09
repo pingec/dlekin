f870424c4662bea11adb8ecd7c7563380de0f7f8
Matej Drolc <pinggo@gmail.com>
2017-03-09 12:17:45 +0100

    Update README.md

--- src/htmlToImage.ts
+++ src/htmlToImage.ts
@@ -1,20 +1,34 @@
 import { execFile } from "child_process";
 
-export function generateImage(config) {
 
-    let input = config.in;
-    let output = config.out;
-    let cmd = config.command;
+export function generateImage(imgConfig, grayscaleConfig, imageGenerated: (err: Error, data: string) => void) {
 
-    let args = ["--zoom", config.zoom,
-        "--height", config.height,
-        "--width", config.width,
+    let input = imgConfig.in;
+    let output = imgConfig.out;
+    let cmd = imgConfig.command;
+
+    let args = ["--zoom", imgConfig.zoom,
+        "--height", imgConfig.height,
+        "--width", imgConfig.width,
+        "--quality", "100",
         "--disable-smart-width",
         input,
         output];
 
-    execFile(cmd, args, (error: any, stdout: any, stderr: any) => {
-        if (error) throw error;
+    execFile(cmd, args, (error, stdout, stderr) => {
+        if (error) imageGenerated(error, null);
+
+        let input = grayscaleConfig.in;
+        let output = grayscaleConfig.out;
+        let cmd = grayscaleConfig.command;
+        let args = [input,
+            "-colors", "16",
+            "-colorspace", "Gray",
+            output];
+
+        execFile(cmd, args, (error: any, stdout: any, stderr: any) => {
+            imageGenerated(error, output);
+        });
     });
 }
 
